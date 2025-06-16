f2f8dfe514f6d93127ed4d1edcc0c5a23ffdc27a
Nebojša Stričević <strika@hackberry.dev>
2025-06-16 07:33:02 +0200

    Implement `tack` part of cycle

--- lib/josif/clock.rb
+++ lib/josif/clock.rb
@@ -2,14 +2,20 @@ module Josif
   class Clock
     def initialize
       @tick_subscribers = []
+      @tack_subscribers = []
     end
 
     def on_tick(&block)
       @tick_subscribers << block
     end
 
+    def on_tack(&block)
+      @tack_subscribers << block
+    end
+
     def cycle
       @tick_subscribers.each(&:call)
+      @tack_subscribers.each(&:call)
     end
   end
 end
--- test/josif/clock_test.rb
+++ test/josif/clock_test.rb
@@ -1,17 +1,28 @@
 require "josif/clock"
 
 describe Josif::Clock do
-  before do
-    @clock = Josif::Clock.new
-    @subscriber = Minitest::Mock.new
-  end
+  describe "#cycle" do
+    before do
+      @clock = Josif::Clock.new
+      @subscriber = Minitest::Mock.new
+    end
 
-  it "calls `tick` on all subscribers" do
-    @subscriber.expect :tick, nil
+    it "calls `tick` on all subscribers" do
+      @subscriber.expect :tick, nil
 
-    @clock.on_tick { @subscriber.tick }
-    @clock.cycle
+      @clock.on_tick { @subscriber.tick }
+      @clock.cycle
 
-    @subscriber.verify
+      @subscriber.verify
+    end
+
+    it "calls `tack` on all subscribers" do
+      @subscriber.expect :tack, nil
+
+      @clock.on_tack { @subscriber.tack }
+      @clock.cycle
+
+      @subscriber.verify
+    end
   end
 end
