a1eff9e0d1036b9374a7db3c6e638c3c99b1e2d6
Nebojša Stričević <strika@hackberry.dev>
2025-07-01 07:21:59 +0200

    Render dummy experiment

--- lib/morf/experiments/dummy/experiment.rb
+++ lib/morf/experiments/dummy/experiment.rb
@@ -1,7 +1,9 @@
 require "morf/grid"
 require "morf/clock"
+require "morf/grid_view"
 require "morf/experiments/dummy/brain"
 require "morf/experiments/dummy/sensor"
+require "morf/experiments/dummy/cell_view"
 
 module Morf
   module Experiments
@@ -10,7 +12,7 @@ module Morf
         def run
           clock = Morf::Clock.new
 
-          Morf::Grid.new(
+          grid = Morf::Grid.new(
             brain_class: Morf::Experiments::Dummy::Brain,
             sensor_class: Morf::Experiments::Dummy::Sensor,
             clock: clock,
@@ -18,8 +20,17 @@ module Morf
             columns: 100
           )
 
+          grid_view = Morf::GridView.new(
+            grid: grid,
+            cell_view_class: Morf::Experiments::Dummy::CellView
+          )
+
+          grid_view.render
+
           10.times do
             clock.cycle
+
+            grid_view.render
 
             sleep 1
           end
