e16912706822f54aa577a460ddeafd6c8dd8f27b
Nebojša Stričević <strika@hackberry.dev>
2025-07-01 07:18:34 +0200

    Implement Dummy::CellView

---
+++ lib/morf/experiments/dummy/cell_view.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "ruby2d"
+
+module Morf
+  module Experiments
+    module Dummy
+      class CellView
+        def initialize(cell:, cell_size:, x:, y:)
+          @cell = cell
+          @cell_size = cell_size
+          @x = x
+          @y = y
+        end
+
+        def render
+          Rectangle.new(
+            x: @x * @cell_size,
+            y: @y * @cell_size,
+            width: @cell_size,
+            height: @cell_size,
+            color: "random"
+          )
+        end
+      end
+    end
+  end
+end
