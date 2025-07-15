0a807b95a650eb746be5de25e8b0a59b34fe5f11
Nebojša Stričević <strika@hackberry.dev>
2025-07-15 07:14:56 +0200

    Use stub object instead of mocks in Cell tests

--- test/morf/cell_test.rb
+++ test/morf/cell_test.rb
@@ -2,10 +2,18 @@ require "test_helper"
 
 describe Morf::Cell do
   before do
-    @brain = Minitest::Mock.new
-    @sensor = Minitest::Mock.new
     @clock = Morf::Clock.new
     @state = 0
+
+    @sensor = Object.new
+    def @sensor.sense
+      0
+    end
+
+    @brain = Object.new
+    def @brain.next_state(_state, _neighbourhood)
+      1
+    end
   end
 
   describe "initialization" do
@@ -21,27 +29,27 @@ describe Morf::Cell do
     end
 
     it "invokes sensor each cycle" do
-      @sensor.expect(:sense, 0)
-      @brain.expect(:next_state, 1, [@state, 0])
+      sensor = Minitest::Mock.new
+      sensor.expect(:sense, 0)
 
       Morf::Cell.new(
         brain: @brain,
-        sensor: @sensor,
+        sensor: sensor,
         clock: @clock,
         state: @state
       )
 
       @clock.cycle
 
-      @sensor.verify
+      sensor.verify
     end
 
     it "invokes brain each cycle" do
-      @brain.expect(:next_state, 1, [@state, 0])
-      @sensor.expect(:sense, 0)
+      brain = Minitest::Mock.new
+      brain.expect(:next_state, 1, [@state, 0])
 
       Morf::Cell.new(
-        brain: @brain,
+        brain: brain,
         sensor: @sensor,
         clock: @clock,
         state: @state
@@ -49,13 +57,10 @@ describe Morf::Cell do
 
       @clock.cycle
 
-      @brain.verify
+      brain.verify
     end
 
     it "moves cell to the new state" do
-      @brain.expect(:next_state, 1, [@state, 0])
-      @sensor.expect(:sense, 0)
-
       cell = Morf::Cell.new(
         brain: @brain,
         sensor: @sensor,
