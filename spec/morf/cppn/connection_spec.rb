13d706a761ffba986f7dc145a6f0f70e4d385046
Nebojša Stričević <strika@hackberry.dev>
2025-08-19 07:22:05 +0200

    Implement skeleton for Morf::CPPN::Connection

---
+++ spec/morf/cppn/connection_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "morf/cppn/node"
+require "morf/cppn/connection"
+
+RSpec.describe Morf::CPPN::Connection do
+  subject(:connection) { described_class.new(input_node: input_node, output_node: output_node, weight: 0.5) }
+
+  let(:input_node) { Morf::CPPN::Node.new(id: 1, layer: :input) }
+  let(:output_node) { Morf::CPPN::Node.new(id: 2, layer: :output) }
+
+  it "has an input node" do
+    expect(connection.input_node).to eq(input_node)
+  end
+
+  it "has an output node" do
+    expect(connection.output_node).to eq(output_node)
+  end
+
+  it "has a weight" do
+    expect(connection.weight).to eq(0.5)
+  end
+end
