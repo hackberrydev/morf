9f718b26201d942d2964f0841c9d57aa37211fc0
Nebojša Stričević <strika@hackberry.dev>
2025-08-27 07:54:12 +0200

    Extract Morf::CPPN::Network#input_nodes and #output_nodes

--- lib/morf/cppn/network.rb
+++ lib/morf/cppn/network.rb
@@ -14,14 +14,19 @@ module Morf
       def evaluate(inputs)
         @nodes.each(&:reset_cache)
 
-        input_nodes = @nodes.select(&:input?)
-        output_nodes = @nodes.select(&:output?)
-
         input_nodes.each_with_index do |node, i|
           node.input = inputs[i]
         end
 
         output_nodes.map(&:calculate)
+      end
+
+      def input_nodes
+        @input_nodes ||= @nodes.select(&:input?)
+      end
+
+      def output_nodes
+        @output_nodes ||= @nodes.select(&:output?)
       end
 
       def populate_incoming_connections
