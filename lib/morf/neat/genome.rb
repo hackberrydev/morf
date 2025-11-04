cf72241086defcd8777296fb5d735dfebf5784fc
Nebojša Stričević <strika@hackberry.dev>
2025-11-04 07:21:26 +0100

    Extract variable in Genome#path_exists?

--- lib/morf/neat/genome.rb
+++ lib/morf/neat/genome.rb
@@ -35,6 +35,7 @@ module Morf
       # Performs a breadth-first search to see if a path exists from `from_id`
       # to `to_id`.
       def path_exists?(from_id, to_id)
+        enabled_connections = @connection_genes.select(&:enabled?)
         stack = [from_id]
         visited = Set.new
 
@@ -44,8 +45,8 @@ module Morf
           next if visited.include?(current_id)
           visited.add(current_id)
 
-          @connection_genes.each do |gene|
-            next unless gene.enabled? && gene.in_node_id == current_id
+          enabled_connections.each do |gene|
+            next unless gene.in_node_id == current_id
 
             out_node_id = gene.out_node_id
             return true if out_node_id == to_id
